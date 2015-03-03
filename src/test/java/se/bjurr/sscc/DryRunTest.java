4d00b144f4e70aeb5ed2baf19512c442c169f274
Tomas Bjerre <tomas.bjerre85@gmail.com>
2015-03-03 18:49:44 +0100

    Fixing save rule bug, better descriptions of fields, new GUI screenshot.

--- src/test/java/se/bjurr/sscc/DryRunTest.java
+++ src/test/java/se/bjurr/sscc/DryRunTest.java
@@ -16,20 +16,34 @@ public class DryRunTest {
  @Test
  public void testThatDryRunAcceptsFaultyCommits() throws IOException {
   refChangeBuilder()
-  .withSetting(SETTING_DRY_RUN, TRUE)
-  .withOneAcceptGroup()
-  .withChangeSet(changeSetBuilder().withId("1").withMessage(COMMIT_MESSAGE_NO_ISSUE).build())
-    .build()
-    .run()
-    .hasTrimmedFlatOutput(
-      "refs/heads/master e2bc4ed003 -> af35d5c1a4  1 Tomas <my@email.com> >>> fixing stuff You need to specity an issue * ((?<!([A-Z]{1,10})-?)[A-Z]+-\\d+)   JIRA")
+    .withSetting(SETTING_DRY_RUN, TRUE)
+    .withOneAcceptGroup()
+    .withChangeSet(changeSetBuilder().withId("1").withMessage(COMMIT_MESSAGE_NO_ISSUE).build())
+  .build()
+  .run()
+  .hasTrimmedFlatOutput(
+    "refs/heads/master e2bc4ed003 -> af35d5c1a4  1 Tomas <my@email.com> >>> fixing stuff You need to specity an issue * ((?<!([A-Z]{1,10})-?)[A-Z]+-\\d+)   JIRA")
+    .wasAccepted();
+ }
+
+ @Test
+ public void testThatDryRunAcceptsFaultyCommitsMessage() throws IOException {
+  refChangeBuilder()
+    .withSetting(SETTING_DRY_RUN, TRUE)
+    .withSetting(SETTING_DRY_RUN_MESSAGE, "In dry run mode!")
+    .withOneAcceptGroup()
+    .withChangeSet(changeSetBuilder().withId("1").withMessage(COMMIT_MESSAGE_NO_ISSUE).build())
+  .build()
+  .run()
+  .hasTrimmedFlatOutput(
+    "refs/heads/master e2bc4ed003 -> af35d5c1a4  1 Tomas <my@email.com> >>> fixing stuff You need to specity an issue * ((?<!([A-Z]{1,10})-?)[A-Z]+-\\d+)   JIRA  In dry run mode!")
     .wasAccepted();
  }
 
  @Test
  public void testThatDryRunCanProvideMessageFromSettings() throws IOException {
   refChangeBuilder().withSetting(SETTING_DRY_RUN, TRUE).withSetting(SETTING_DRY_RUN_MESSAGE, "In dry run mode!")
-  .withOneAcceptGroup().withChangeSet(changeSetBuilder().withId("1").withMessage(COMMIT_MESSAGE_NO_ISSUE).build())
-  .build().run().hasOutputFrom("testThatDryRunCanProvideMessageFromSettings.txt").wasAccepted();
+    .withOneAcceptGroup().withChangeSet(changeSetBuilder().withId("1").withMessage(COMMIT_MESSAGE_NO_ISSUE).build())
+    .build().run().hasOutputFrom("testThatDryRunCanProvideMessageFromSettings.txt").wasAccepted();
  }
 }
