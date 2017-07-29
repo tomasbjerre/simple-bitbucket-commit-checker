eb73ed3dd0b1ced909d8892dcdc3ff8920dc34df
Tomas Bjerre <tomas.bjerre85@gmail.com>
2017-07-29 17:36:03 +0200

    Namespacing CSS and JS
     * Terrible docs from Atlassians forces this ugly solution...

--- src/main/resources/js/sbcc.js
+++ src/main/resources/js/sbcc.js
@@ -9,24 +9,24 @@
   return itemIndex;
  }
 
- $('.removeGroup').live('click',function() {
+ $('.sbcc.removeGroup').live('click',function() {
   $group = $(this).closest('.group');
   $group.remove();
  });
 
- $('.newGroup').live('click',function() {
+ $('.sbcc.newGroup').live('click',function() {
   $sbcc = $(this).closest('.sbcc');
   $groups = $('.groups',$sbcc);
   var groupIndex = highestIndex($('.group',$groups)) + 1;
   $groups.prepend(se.bjurr.group({'group':groupIndex,'config':[],'errors':[]}));
  });
 
- $('.removeRule').live('click',function() {
+ $('.sbcc.removeRule').live('click',function() {
   $rule = $(this).closest('.rule');
   $rule.remove();
  });
 
- $('.newRule').live('click',function() {
+ $('.sbcc.newRule').live('click',function() {
   $group = $(this).closest('.group');
   var groupIndex = $group.data('index');
   $rules = $('.rules',$group);
